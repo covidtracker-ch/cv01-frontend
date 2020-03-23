22050ae6ec1ec56a72b303878ece9d9faf0a80bc
Samuel Alder <samuel.alder@gmail.com>
2020-03-23 21:13:22 +0100

    All Translations

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,5 +1,8 @@
 var formBtn = document.getElementById('btn-send');
 var form = document.getElementById('covid-form');
+var formExpansion = document.getElementById('form-expanded');
+var healthyRadio = document.getElementById('feelsHealthy-1');
+var sickRadio = document.getElementById('feelsHealthy-0');
 
 function sendForm() {
   console.log(form);
@@ -7,8 +10,17 @@ function sendForm() {
   console.log(...fd);
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  sendForm();
-});
+if(form) {  
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+  
+    sendForm();
+  });
+  
+  sickRadio.addEventListener('change', e => {
+    formExpansion.classList.add('visible');
+  });
+  healthyRadio.addEventListener('change', e => {
+    formExpansion.classList.remove('visible');
+  });
+}

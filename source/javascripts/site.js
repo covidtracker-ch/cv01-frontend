6e7c292a7a550183035ce29dd237116e162408a0
Samuel Alder <samuel.alder@gmail.com>
2020-03-23 23:43:31 +0100

    Form improvements and translations

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,9 +3,9 @@ var form = document.getElementById('covid-form');
 var formExpansion = document.getElementById('form-expanded');
 var healthyRadio = document.getElementById('feelsHealthy-1');
 var sickRadio = document.getElementById('feelsHealthy-0');
+var toggles = document.querySelectorAll('.js-toggle');
 
 function sendForm() {
-  console.log(form);
   var fd = new FormData(form);
   console.log(...fd);
 }
@@ -23,4 +23,15 @@ if(form) {
   healthyRadio.addEventListener('change', e => {
     formExpansion.classList.remove('visible');
   });
+
+  for(var toggle of toggles) {
+    toggle.addEventListener('change', e => {
+      if(e.target.dataset.hide) {
+        document.getElementById(e.target.dataset.hide).classList.add('hidden');
+      }
+      if(e.target.dataset.show) {
+        document.getElementById(e.target.dataset.show).classList.remove('hidden');
+      }
+    });
+  }
 }

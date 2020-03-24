41c878c06c77c487627e80bd96cb694ab00ae40b
Samuel Alder <samuel.alder@gmail.com>
2020-03-24 18:40:10 +0100

    Fixed expanded form toggle

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,17 +1,19 @@
 var form = document.getElementById('covid-form');
 var toggles = document.querySelectorAll('.js-toggle');
 var hiddens = document.querySelectorAll('.js-hide');
+var langLinks = document.querySelectorAll('.js-setLang');
 
 function checkLang() {
   var lang = window.navigator.language.slice(0, 2);
-
+  console.log(window.location.protocol);
+  
   if(lang == 'fr') {
-    window.location.assign(window.location.hostname + '/fr' + window.location.pathname);
+    //window.location.assign(window.location.protocol + '//' + window.location.hostname + '/fr' + window.location.pathname);
   }else if(lang == 'it') {
-    window.location.assign(window.location.hostname + '/it' + window.location.pathname);
+    window.location.assign(window.location.protocol + '//' + window.location.hostname + '/it' + window.location.pathname);
   }
 }
-checkLang();
+//checkLang();
 
 if(form) {    
   for(var i=0; i < hiddens.length; i++) {
@@ -27,6 +29,12 @@ if(form) {
       if(e.target.dataset.show) {
         document.getElementById(e.target.dataset.show).classList.remove('hidden');
       }
+    });
+  }
+
+  for(var i=0; i < langLinks.length; i++) {
+    langLinks[i].addEventListener('click', function(e) {
+      console.log('lang');
     });
   }
 }

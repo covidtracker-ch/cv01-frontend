de25ff394c4eb4e6a2183b4d074e48a136d2d9a2
Samuel Alder <samuel.alder@gmail.com>
2020-03-24 15:23:43 +0100

    Added polyfills

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -12,26 +12,39 @@ for(var hidden of hiddens) {
 }
 
 function sendForm() {
+  var xhr = new XMLHttpRequest();
   var fd = new FormData(form);
   console.log(...fd);
+  
+  xhr.addEventListener('load', function(e) {
+    console.log(e.target.responsText);
+  });
+  
+  xhr.addEventListener('error', function(e) {
+    console.log(e);
+  });
+  
+  xhr.open('POST', form.action);
+  xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+  xhr.send(fd);
 }
 
 if(form) {  
-  form.addEventListener('submit', e => {
+  form.addEventListener('submit', function(e) {
     e.preventDefault();
   
     sendForm();
   });
   
-  sickRadio.addEventListener('change', e => {
+  sickRadio.addEventListener('change', function(e) {
     formExpansion.classList.remove('hidden');
   });
-  healthyRadio.addEventListener('change', e => {
+  healthyRadio.addEventListener('change', function(e) {
     formExpansion.classList.add('hidden');
   });
 
   for(var toggle of toggles) {
-    toggle.addEventListener('change', e => {
+    toggle.addEventListener('change', function(e) {
       if(e.target.dataset.hide) {
         document.getElementById(e.target.dataset.hide).classList.add('hidden');
       }

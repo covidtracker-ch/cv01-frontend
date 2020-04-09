ef4df3633bee597b967c6563dda4b8a685b07dd7
faisala <alquaddoomi@nexus.ethz.ch>
2020-04-09 10:26:54 +0200

    Reverts minified code to un-minified code. Comments out currently problematic goToLang() function that causes a redirect loop.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,150 +1,279 @@
+var form = document.getElementById('covid-form');
+var toggles = document.querySelectorAll('.js-toggle');
+var hiddens = document.querySelectorAll('.js-hide');
+var langLinks = document.querySelectorAll('.js-setLang');
+
 function searchParams() {
-  var e, t, n = window.location.search.substring(1).split("&"), a = {};
-  for (t in n)
-      "" !== n[t] && (e = n[t].split("="),
-      a[decodeURIComponent(e[0])] = decodeURIComponent(e[1]));
-  return a
+  var pairs = window.location.search.substring(1).split("&"), obj = {}, pair, i;
+  for (i in pairs) {
+    if ( pairs[i] === "" ) { continue }
+    pair = pairs[i].split("=");
+    obj[ decodeURIComponent( pair[0] ) ] = decodeURIComponent( pair[1] );
+  }
+  return obj;
 }
+
 function checkForCode() {
-  var e, t = searchParams();
-  t.code ? (e = t.code,
-  localStorage.setItem("lastCode", e),
-  replaceKeyInStore(REPLACE_SENTINEL, e)) : e = localStorage.getItem("lastCode");
-  var n = document.getElementById("yourCodeHere");
-  n && (n.innerText = e)
+  var params = searchParams();
+  var lastCode;
+
+  if (params['code']) {
+    // set this code as the last-used code
+    lastCode = params['code'];
+    localStorage.setItem('lastCode', lastCode);
+
+    // search through formData and replace any keys we deferred with the new code
+    replaceKeyInStore(REPLACE_SENTINEL, lastCode);
+  }
+  else {
+    lastCode = localStorage.getItem('lastCode');
+  }
+
+  // if there's an element that displays a code, fill it with the current one
+  var yourCodeHere = document.getElementById('yourCodeHere');
+  if (yourCodeHere) {
+    yourCodeHere.innerText = lastCode;
+  }
 }
+
 function checkError() {
-  "true" === searchParams().error && (document.getElementById("msg-error").classList.remove("hidden"),
-  deleteKeyInStore(REPLACE_SENTINEL))
+  var params = searchParams();
+  if (params['error'] === 'true') {
+    document.getElementById('msg-error').classList.remove('hidden');
+
+    // clean up the unfinished form data, since we don't have a code for it
+    // FIXME: is this the right behavior, or should we just reload the form with this data?
+    deleteKeyInStore(REPLACE_SENTINEL);
+  }
 }
-function goToLang(e) {
-  //var t = window.location.pathname.split("/").pop();
-  //"de" == e ? window.location.assign(window.location.origin + "/" + t) : window.location.assign(window.location.origin + "/" + e + "/" + t)
+
+function goToLang(lang) {
+  // var path = window.location.pathname.split("/").pop();
+  // if(lang == 'de') {
+  //   window.location.assign(window.location.origin + '/' + path);
+  // }else{
+  //   window.location.assign(window.location.origin + '/' + lang + '/' + path);
+  // }
 }
 function checkLang() {
-  var e = navigator.language || navigator.userLanguage;
-  e = e.slice(0, 2);
-  var t = document.body.dataset.lang
-    , n = localStorage.getItem("languageOverwrite");
-  if (e != t) {
-      if (n) {
-          if (n == t)
-              return;
-          return void goToLang(n)
-      }
-      "fr" == e ? (localStorage.setItem("languageOverwrite", "fr"),
-      goToLang("fr")) : "it" == e ? (localStorage.setItem("languageOverwrite", "it"),
-      goToLang("it")) : "en" == e && (localStorage.setItem("languageOverwrite", "en"),
-      goToLang("en"))
+  var userLang = navigator.language || navigator.userLanguage;
+  userLang = userLang.slice(0,2);
+  var currentLang = document.body.dataset.lang;
+  var overwrittenLang = localStorage.getItem('languageOverwrite');
+
+  if(userLang == currentLang) {
+    return;
+  }
+
+  if(overwrittenLang) {
+    if(overwrittenLang == currentLang) return;
+    goToLang(overwrittenLang);
+    return;
+  }
+
+  if(userLang == 'fr') {
+    localStorage.setItem('languageOverwrite', 'fr');
+    goToLang('fr');
+  } else if(userLang == 'it') {
+    localStorage.setItem('languageOverwrite', 'it');
+    goToLang('it');
+  } else if(userLang == 'en') {
+    localStorage.setItem('languageOverwrite', 'en');
+    goToLang('en');
   }
 }
+
 function setupForm() {
-  if (form) {
-      for (var e = 0; e < hiddens.length; e++)
-          hiddens[e].classList.remove("js-hide"),
-          hiddens[e].classList.add("hidden");
-      for (var e = 0; e < toggles.length; e++) {
-          var t = "A" === toggles[e].tagName.toUpperCase() ? "click" : "input";
-          toggles[e].addEventListener(t, function(e) {
-              "click" === e.type && e.preventDefault(),
-              e.target.dataset.hide && document.getElementById(e.target.dataset.hide).classList.add("hidden"),
-              e.target.dataset.show && document.getElementById(e.target.dataset.show).classList.remove("hidden")
-          })
-      }
-      for (var e = 0; e < langLinks.length; e++)
-          langLinks[e].addEventListener("click", function(e) {
-              localStorage.setItem("languageOverwrite", e.currentTarget.dataset.lang)
-          })
-  }
-}
-function updateTotalSubs(e) {
-  var t = new XMLHttpRequest;
-  t.open("GET", "https://api.covidtracker.ch/count"),
-  t.send(),
-  t.onload = function() {
-      var n = t.responseText;
-      n = n.slice(0, 3) + "'" + n.slice(3),
-      document.getElementById(e).innerHTML = n
-  }
-}
-function randomChoice(e) {
-  return e[Math.floor(Math.random() * e.length)]
-}
-function retrieveCatImage(e) {
-  var t = randomChoice(haired_breeds)
-    , n = new XMLHttpRequest;
-  n.open("GET", "https://api.thecatapi.com/v1/images/search?size=thumb&breed_id=" + t),
-  n.setRequestHeader("x-api-key", "9f8170a1-4a5a-4568-b120-795cd63809d5"),
-  n.send(),
-  n.onload = function() {
-      var a = JSON.parse(n.responseText);
-      document.getElementById(e).innerHTML = '<img alt="' + t + '_cat_img" src="' + a[0].url + '"  />'
-  }
-}
-function getLastCode() {
-  var e = localStorage.getItem("lastCode");
-  if (!e) {
-      var t = JSON.parse(localStorage.getItem("formData"));
-      for (var n in t)
-          if (t.hasOwnProperty(n)) {
-              e = n;
-              break
-          }
-  }
-  return e
-}
-function bindParticipantsList(e) {
-  function t(e) {
-      var t = document.getElementById("participantCodeManualBox");
-      "__none__" === e ? (clearForm(),
-      t.classList.remove("hidden")) : (rehydrateForm(e),
-      t.classList.add("hidden"))
-  }
-  var n, a, o = document.getElementById("participantCodeList");
-  if (o) {
-      var r = JSON.parse(localStorage.getItem("formData"));
-      if (r)
-          for (n in r)
-              r.hasOwnProperty(n) && (a = document.createElement("option"),
-              a.innerText = n,
-              a.setAttribute("value", n),
-              o.appendChild(a),
-              e && n === e && (a.selected = !0));
-      t(o.value),
-      o.addEventListener("input", function(e) {
-          t(e.target.value)
+  if(form) {
+    for(var i=0; i < hiddens.length; i++) {
+      hiddens[i].classList.remove('js-hide');
+      hiddens[i].classList.add('hidden');
+    }
+
+    for(var i=0; i < toggles.length; i++) {
+      var event = toggles[i].tagName.toUpperCase() === 'A' ? 'click' : 'input';
+
+      toggles[i].addEventListener(event, function(e) {
+        if (e.type === 'click') {
+          e.preventDefault();
+        }
+
+        if (e.target.dataset.hide) {
+          document.getElementById(e.target.dataset.hide).classList.add('hidden');
+        }
+        if (e.target.dataset.show) {
+          document.getElementById(e.target.dataset.show).classList.remove('hidden');
+        }
       });
-      var i = document.getElementById("firstTimeSurvey-1")
-        , c = document.getElementById("firstTimeSurvey-0");
-      i && c && (i.addEventListener("click", function() {
-          clearForm()
-      }),
-      c.addEventListener("click", function() {
-          t(o.value)
-      }))
+    }
+
+    for(var i=0; i < langLinks.length; i++) {
+      langLinks[i].addEventListener('click', function(e) {
+        localStorage.setItem('languageOverwrite', e.currentTarget.dataset.lang);
+      });
+    }
   }
 }
+
+function updateTotalSubs(targetID) {
+  var http = new XMLHttpRequest();
+  http.open("GET", "https://api.covidtracker.ch/count");
+  http.send();
+  http.onload = function() {
+    var s = http.responseText;
+    s = s.slice(0, 3) + "'" + s.slice(3);
+    document.getElementById(targetID).innerHTML = s;
+  }
+}
+
+// cat API data + methods
+var haired_breeds = [
+  "abys", "aege", "abob", "acur", "asho", "awir", "amau", "amis",
+  "bali", "beng", "birm", "bomb", "bslo", "bsho", "bure", "buri",
+  "cspa", "ctif", "char", "chau", "chee", "csho", "crex", "cymr",
+  "cypr", "drex", "lihu", "emau", "ebur", "esho", "hbro", "hima",
+  "jbob", "java", "khao", "kora", "kuri", "lape", "mcoo", "mala",
+  "manx", "munc", "nebe", "norw", "ocic", "orie", "pers", "pixi",
+  "raga", "ragd", "rblu", "sava", "sfol", "srex", "siam", "sibe",
+  "sing", "snow", "soma", "tonk", "toyg", "tang", "tvan", "ycho"
+];
+
+function randomChoice(samples) {
+  var idx = Math.floor(Math.random() * samples.length);
+  return samples[idx];
+}
+
+function retrieveCatImage(targetID) {
+  var cat_breed = randomChoice(haired_breeds);
+  var http = new XMLHttpRequest();
+  http.open("GET", "https://api.thecatapi.com/v1/images/search?size=thumb&breed_id=" + cat_breed);
+  http.setRequestHeader('x-api-key', '9f8170a1-4a5a-4568-b120-795cd63809d5');
+  http.send();
+  http.onload = function() {
+    var data = JSON.parse(http.responseText);
+    document.getElementById(targetID).innerHTML = "<img " +
+        "alt=\"" + cat_breed + "_cat_img\" " +
+        "src=\"" + data[0].url +"\" " +
+        // "width=\"" + data[0].width + "\" " +
+        // "height=\"" + data[0].width + "\"" +
+        " />"
+  }
+}
+
+// ---------------------------------------------------------------------------------------------
+// --- participant code handling
+// ---------------------------------------------------------------------------------------------
+
+function getLastCode() {
+  var lastCode = localStorage.getItem('lastCode');
+
+  if (!lastCode) {
+    var allResponses = JSON.parse(localStorage.getItem('formData'));
+
+    // since no code was specified, fetch the first entry in formData
+    // (i'm using this silly for loop b/c Object.values() doesn't exist in IE*)
+    for (var code in allResponses) {
+      if (!allResponses.hasOwnProperty(code))
+        continue;
+      lastCode = code;
+      break;
+    }
+  }
+
+  return lastCode;
+}
+
+function bindParticipantsList(lastCode) {
+  // populate the participant dropdown with values
+  var partElem = document.getElementById('participantCodeList'), code, newOption;
+
+  if (!partElem) {return;}
+
+  var allResponses = JSON.parse(localStorage.getItem('formData'));
+
+  if (allResponses) {
+    for (code in allResponses) {
+      if (!allResponses.hasOwnProperty(code))
+        continue;
+
+      newOption = document.createElement("option");
+      newOption.innerText = code;
+      newOption.setAttribute('value', code);
+      partElem.appendChild(newOption);
+
+      if (lastCode && code === lastCode) {
+        newOption.selected = true;
+      }
+    }
+  }
+
+  // applies the participant code selection to the form's contents
+  function updateDropdownBinding(code) {
+    var manualCode = document.getElementById('participantCodeManualBox');
+
+    if (code === "__none__") {
+      clearForm();
+      manualCode.classList.remove("hidden");
+    }
+    else {
+      rehydrateForm(code);
+      manualCode.classList.add("hidden");
+    }
+  }
+
+  // and run the selection logic once to sync the form
+  updateDropdownBinding(partElem.value);
+
+  partElem.addEventListener('input', function(ev) {
+    updateDropdownBinding(ev.target.value);
+  });
+
+  // make selecting the 'yes' response for the "first time?" prompt clear the form
+  // make selecting the 'no' response reload the form if there's a selection
+
+  var yesResponse = document.getElementById('firstTimeSurvey-1');
+  var noResponse = document.getElementById('firstTimeSurvey-0');
+  if (yesResponse && noResponse) {
+    yesResponse.addEventListener('click', function() {
+      clearForm();
+    });
+    noResponse.addEventListener('click', function() {
+      updateDropdownBinding(partElem.value);
+    });
+  }
+}
+
 function bindConsentButtons() {
-  function e() {
-      a.disabled = !t.checked
+  var yesResponse = document.getElementById('consentToStudy-1');
+  var noResponse = document.getElementById('consentToStudy-0');
+  var submitBtn = document.getElementById('btn-send');
+
+  function syncSubmit() {
+    submitBtn.disabled = !(yesResponse.checked);
   }
-  var t = document.getElementById("consentToStudy-1")
-    , n = document.getElementById("consentToStudy-0")
-    , a = document.getElementById("btn-send");
-  a && t && n && (e(),
-  t.addEventListener("click", e),
-  n.addEventListener("click", e))
+
+  if (submitBtn && yesResponse && noResponse) {
+    syncSubmit();
+    yesResponse.addEventListener('click', syncSubmit);
+    noResponse.addEventListener('click', syncSubmit);
+  }
 }
-var form = document.getElementById("covid-form")
-, toggles = document.querySelectorAll(".js-toggle")
-, hiddens = document.querySelectorAll(".js-hide")
-, langLinks = document.querySelectorAll(".js-setLang")
-, haired_breeds = ["abys", "aege", "abob", "acur", "asho", "awir", "amau", "amis", "bali", "beng", "birm", "bomb", "bslo", "bsho", "bure", "buri", "cspa", "ctif", "char", "chau", "chee", "csho", "crex", "cymr", "cypr", "drex", "lihu", "emau", "ebur", "esho", "hbro", "hima", "jbob", "java", "khao", "kora", "kuri", "lape", "mcoo", "mala", "manx", "munc", "nebe", "norw", "ocic", "orie", "pers", "pixi", "raga", "ragd", "rblu", "sava", "sfol", "srex", "siam", "sibe", "sing", "snow", "soma", "tonk", "toyg", "tang", "tvan", "ycho"];
+
+// ---------------------------------------------------------------------------------------------
+// --- execute on complete DOM load
+// ---------------------------------------------------------------------------------------------
+
 onDOMReady(function() {
-  checkError(),
-  checkLang(),
-  checkForCode(),
-  setupForm(),
-  bindParticipantsList(getLastCode()),
-  bindConsentButtons()
+  checkError();
+  checkLang();
+  checkForCode();
+
+  setupForm();
+
+  var lastCode = getLastCode();
+
+  // binding the participants' list will rehydrate
+  // the form when it preselects the last participant
+  bindParticipantsList(lastCode);
+  bindConsentButtons();
 });

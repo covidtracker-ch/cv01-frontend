a40fccb7743d24cd02fb8c053e8cd7874bc26ded
faisala <alquaddoomi@nexus.ethz.ch>
2020-04-09 17:19:27 +0200

    Language links now redirect to the current page with the newly-chosen language.

--- config.rb
+++ config.rb
@@ -34,11 +34,11 @@ page '/*.txt', layout: false
 # Methods defined in the helpers block are available in templates
 # https://middlemanapp.com/basics/helper-methods/
 
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
+helpers do
+  def curPageWithoutLang
+    return current_page.path.sub("index.html", "").sub(I18n.locale.to_s + "/", "")
+  end
+end
 
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
